e3c8ba8c24c815700d30e2cc23305a05d5d225bf
tomcoakes <tomcoakes@gmail.com>
2020-07-24 15:01:32 +0100

    add test cases for unknown items

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -2,11 +2,18 @@ import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
-
-    it('should foo', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('foo');
+  describe('unknown items', function () {
+    it('should not update the quality for unknown items', function() {
+      const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).to.equal('foo');
+      expect(items[0].quality).to.equal(0);
     });
 
+    it('should reduce the sellIn by 1 for unknown items', function() {
+      const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].sellIn).to.equal(-1);
+    });
+  });
 });
