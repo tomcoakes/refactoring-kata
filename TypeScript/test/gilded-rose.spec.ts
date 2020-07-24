1eb33977d7ac299550eda0c5dc7844c984c0b1e1
tomcoakes <tomcoakes@gmail.com>
2020-07-24 15:23:28 +0100

    add table driven tests

--- TypeScript/test/gilded-rose.spec.ts
+++ TypeScript/test/gilded-rose.spec.ts
@@ -3,17 +3,27 @@ import { Item, GildedRose } from '../app/gilded-rose';
 
 describe('Gilded Rose', function () {
   describe('unknown items', function () {
-    it('should not update the quality for unknown items', function() {
-      const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].name).to.equal('foo');
-      expect(items[0].quality).to.equal(0);
-    });
-
-    it('should reduce the sellIn by 1 for unknown items', function() {
-      const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-      const items = gildedRose.updateQuality();
-      expect(items[0].sellIn).to.equal(-1);
-    });
+    [
+      { quality: 0, sellIn: 0, expectedQuality: 0, expectedSellIn: -1},
+      { quality: 50, sellIn: 0, expectedQuality: 48, expectedSellIn: -1},
+      { quality: 51, sellIn: 0, expectedQuality: 49, expectedSellIn: -1},
+      { quality: 0, sellIn: 1, expectedQuality: 0, expectedSellIn: 0},
+      { quality: 50, sellIn: 1, expectedQuality: 49, expectedSellIn: 0},
+      { quality: 51, sellIn: 1, expectedQuality: 50, expectedSellIn: 0},
+      { quality: 0, sellIn: 11, expectedQuality: 0, expectedSellIn: 10},
+      { quality: 50, sellIn: 11, expectedQuality: 49, expectedSellIn: 10},
+      { quality: 0, sellIn: 12, expectedQuality: 0, expectedSellIn: 11},
+      { quality: 50, sellIn: 12, expectedQuality: 49, expectedSellIn: 11},
+      { quality: 0, sellIn: 7, expectedQuality: 0, expectedSellIn: 6},
+      { quality: 50, sellIn: 7, expectedQuality: 49, expectedSellIn: 6},
+    ].forEach(({ quality, sellIn, expectedQuality, expectedSellIn} ) => {
+      it('sets the correct quality', function() {
+        const gildedRose = new GildedRose([ new Item('foo', sellIn, quality) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].name).to.equal('foo');
+        expect(items[0].quality).to.equal(expectedQuality);
+        expect(items[0].sellIn).to.equal(expectedSellIn);
+      });
+    })
   });
 });
